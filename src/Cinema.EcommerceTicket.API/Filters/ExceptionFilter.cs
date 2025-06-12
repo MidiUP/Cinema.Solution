8a7b10b428b615878b34a98289d88fe998a68b7e
Mateus Mendes <mateusmendesm16@outlook.com>
2025-06-12 07:12:51 -0300

    (refactor): ajuste em Exception Filter da API

--- src/Cinema.EcommerceTicket.API/Filters/ExceptionFilter.cs
+++ src/Cinema.EcommerceTicket.API/Filters/ExceptionFilter.cs
@@ -2,37 +2,35 @@
 using Cinema.EcommerceTicket.Domain.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
 using System.Net;
 
 namespace Cinema.EcommerceTicket.API.Filters;
 
-public class ExceptionFilter : IExceptionFilter
+public class ExceptionFilter(ILogger<ExceptionFilter> logger) : IExceptionFilter
 {
+    private readonly ILogger<ExceptionFilter> _logger = logger;
+
     const string SERVER_ERROR_MESSAGE = "Ocorreu um inesperado. Por favor tente novamente mais tarde";
 
     public void OnException(ExceptionContext context)
     {
-        if (context.Exception is CinemaEcommerceTicketException exception)
-            HandleResultException(context, exception.ERROR_CODE);
+        if (context.Exception is CinemaEcommerceTicketException cinemaEcommerceTicketException)
+        {
+            _logger.LogWarning(context.Exception, "Erro conhecido no filtro de exceção");
+            HandleResultException(context, new ErrorResponseDto(cinemaEcommerceTicketException.Errors ?? [], cinemaEcommerceTicketException.Message), cinemaEcommerceTicketException.ERROR_CODE);
+        }
         else
-            HandleUnknownException(context);
+        {
+            _logger.LogError(context.Exception, "Erro desconhecido no filtro de exceção");
+            HandleResultException(context, new ErrorResponseDto(SERVER_ERROR_MESSAGE), (int)HttpStatusCode.InternalServerError);
+        }
     }
 
-    private static void HandleResultException(ExceptionContext context, int statusCode)
+    private static void HandleResultException(ExceptionContext context, ErrorResponseDto errorResponseDto, int statusCode)
     {
-        var exception = context.Exception;
-
-        if(exception is CinemaEcommerceTicketException cinemaAPIGatewayException)
-            context.Result = new ObjectResult(new ErrorResponseDto(cinemaAPIGatewayException.Errors ?? [], cinemaAPIGatewayException.Message));
-        else
-            context.Result = new ObjectResult(new ErrorResponseDto(SERVER_ERROR_MESSAGE));
-            
+        context.Result = new ObjectResult(errorResponseDto);
         context.HttpContext.Response.StatusCode = statusCode;
-    }
-
-    private static void HandleUnknownException(ExceptionContext context)
-    {
-        HandleResultException(context, (int)HttpStatusCode.InternalServerError);
     }
 
 }
