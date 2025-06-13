ac2d606e54f5ae2a157fb361ddfc6c59a6c5521b
Mateus Mendes <mateusmendesm16@outlook.com>
2025-06-12 23:42:35 -0300

    Squashed 'Cinema.EcommerceTicket/' changes from 8a7b10b..39b99b7
    39b99b7 (feat): implementação de estrutura http ao projeto
    
    git-subtree-dir: Cinema.EcommerceTicket
    git-subtree-split: 39b99b7ae390a3920e8a9883501b83403379fca4

---
+++ src/Cinema.EcommerceTicket.Infrastructure/HttpClients/GatewayAdapters/GatewayAdapterBase.cs
@@ -0,0 +1,7 @@
+﻿namespace Cinema.EcommerceTicket.Infrastructure.HttpClients.GatewayAdapters;
+
+public abstract class GatewayAdapterBase
+{
+    public abstract HttpClient Client { get; set; }
+    public abstract Task Authenticate(HttpRequestMessage requestMessage);
+}
---
+++ src/Cinema.EcommerceTicket.Infrastructure/HttpClients/Setup.cs
@@ -0,0 +1,37 @@
+﻿using Cinema.EcommerceTicket.Domain.Shared;
+using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Extensions.Http;
+
+namespace Cinema.EcommerceTicket.Infrastructure.HttpClients;
+
+public static class Setup
+{
+    public static void AddHttpClients(this IServiceCollection services)
+    {
+        services.AddCatalogApiHttpClient();
+        //services.AddHandlersHttp(); exemplo de como adicionar handlers personalizados
+    }
+
+    private static void AddCatalogApiHttpClient(this IServiceCollection services)
+    {
+        services.AddHttpClient(Constants.CatalogApi.NAME, httpClient =>
+        {
+            httpClient.BaseAddress = new Uri(Constants.CatalogApi.BASE_URL);
+        })
+        .AddPolicyHandler(GetRetryPolicy());
+        //.AddHttpMessageHandler<AuthenticateCustomHandler<SensediaGatewayAdapter>>();
+    }
+
+    private static void AddHandlersHttp(this IServiceCollection services)
+    {
+        //services.AddTransient<AuthenticateCustomHandler<SensediaGatewayAdapter>>(); // exemplo de como adicionar handler de autenticação personalizado
+    }
+
+    private static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+    {
+        return HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .WaitAndRetryAsync(3, attempt => TimeSpan.FromSeconds(Math.Pow(2, attempt)));
+    }
+}
